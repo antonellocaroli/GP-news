07afd6b1f9d16ed9ee154ce7e4978d4f305cc632
antonellocaroli <antonellocaroli@gmail.com>
2020-02-27 06:15:00 +0100

    up

--- news.sh
+++ news.sh
@@ -10,6 +10,23 @@ echo -e "$BBlack Latest News:$Color_Off"
 echo
 ###############################################
 echo -e " \e[38;5;154m────────────────────────────────────────────\e[0m"
+echo -e "$BGreen 25.02.19$Color_Off $BBlack A new version of MyMpd is available v.6.2.1$Color_Off"
+echo
+echo -e "https://github.com/jcorporation/myMPD/releases/tag/v6.2.1"
+echo
+echo -e "to update:
+$BBlack gp-update$Color_Off
+ and
+ ssh: command $BBlack mympd-up$Color_Off $BGreen(faster)$Color_Off
+ or
+ web-interface > System H. Software Upd/Rem > Select Software Update > MyMpd > EXCUTE $BRed(slow)$Color_Off
+ or
+ web-interface > System A. Excute Command > mympd-up > EXCUTE $BGreen(faster)$Color_Off"
+echo -e " \e[38;5;154m────────────────────────────────────────────\e[0m"
+
+###########################################
+###############################################
+echo -e " \e[38;5;154m────────────────────────────────────────────\e[0m"
 echo -e "$BGreen 26.02.19$Color_Off $BBlack A new version Kernel no-RT is aviable for$BGreen Rpi4$Color_Off v.4.19.106 $Color_Off"
 echo
 echo
