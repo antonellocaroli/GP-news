14fda0f11299a3cdec599bbe068579c7b91a548d
antonellocaroli <antonellocaroli@gmail.com>
2020-02-27 11:05:37 +0100

    Update news.sh

--- news.sh
+++ news.sh
@@ -2,8 +2,8 @@
 . /opt/.gentooplayer/function/felenco.sh
 gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
 gpmodel=$(sed -n 16p /opt/.gentooplayer/GentooPlayer/gentooplayer/.hw_model)
-
-
+#
+#
 echo -e "$BRed GentooPlayer $Color_Off $BBlue $gpmodel $Color_Off $BBlack version=$gpversion $Color_Off"
 echo
 echo -e "$BBlack Latest News:$Color_Off"
