bebb199cb53e3080c089767517e8f3adfa11673b
antonellocaroli <antonellocaroli@gmail.com>
2020-02-27 05:50:57 +0100

    up

--- news.sh
+++ news.sh
@@ -10,6 +10,20 @@ echo -e "$BBlack Latest News:$Color_Off"
 echo
 ###############################################
 echo -e " \e[38;5;154m────────────────────────────────────────────\e[0m"
+echo -e "$BGreen 26.02.19$Color_Off $BBlack A new version Kernel no-RT is aviable for Rpi4 v.4.19.106 $Color_Off"
+echo
+echo
+echo -e "to update:
+$BBlack gp-update$Color_Off
+ and
+ ssh: command $BBlack kernelupsw$Color_Off
+ or
+ web-interface > System A. UP/DW kernel > List Kernel Version > 4.19.106-260220 > EXCUTE"
+echo -e " \e[38;5;154m────────────────────────────────────────────\e[0m"
+
+###########################################
+###############################################
+echo -e " \e[38;5;154m────────────────────────────────────────────\e[0m"
 echo -e "$BGreen 25.02.19$Color_Off $BBlack A new version of MyMpd is available v.6.2.0$Color_Off"
 echo
 echo -e "Changelog: https://github.com/jcorporation/myMPD/releases/tag/v6.2.0"
