b50613d136b9fb1620027dad55338f6dc15e049d
antonellocaroli <antonellocaroli@gmail.com>
2020-03-07 10:20:35 +0100

    up

--- news.sh
+++ news.sh
@@ -116,13 +116,14 @@ $BBlack gp-update$Color_Off
 echo -e " ────────────────────────────────────────────"
 ###########################################
 ###############################################
-#echo -e " ────────────────────────────────────────────"
-#echo -e "$BGreen 07.03.20$Color_Off $BBlack x86_64 Update versione 2.10 to 2.11$Color_Off"
-#echo
-#echo -e "to update:
-#$BBlack gp-update$Color_Off
-# web-interface > System H. GP-Version Update > Update version > 2.10-up-2.11 > EXCUTE"
-#echo -e " ────────────────────────────────────────────"
+echo -e " ────────────────────────────────────────────"
+echo -e "$BGreen 07.03.20$Color_Off $BBlack x86_64 Update versione 2.10 to 2.11$Color_Off"
+echo "Depending on the speed of the CPU and/or optical media it can take a long time.."
+echo
+echo -e "to update:
+$BBlack gp-update$Color_Off
+ web-interface > System H. GP-Version Update > Update version > 2.10-up-2.11 > EXCUTE"
+echo -e " ────────────────────────────────────────────"
 ###########################################
 
 ############
