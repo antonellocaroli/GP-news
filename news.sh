e225600091c0fa2861979e97762a8ac860265c04
antonellocaroli <antonellocaroli@gmail.com>
2020-03-07 08:46:15 +0100

    up

--- news.sh
+++ news.sh
@@ -121,7 +121,7 @@ echo -e "$BGreen 07.03.20$Color_Off $BBlack x86_64 Update versione 2.10 to 2.11$
 echo
 echo -e "to update:
 $BBlack gp-update$Color_Off
- web-interface > System H. GP-Version Update > Update version > 2.10-up-2.11 > EXCUTE
+ web-interface > System H. GP-Version Update > Update version > 2.10-up-2.11 > EXCUTE"
 echo -e " ────────────────────────────────────────────"
 ###########################################
 
