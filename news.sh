feb2c1c55726f1ab257401a5f3db624bef70c829
antonellocaroli <antonellocaroli@gmail.com>
2022-06-02 07:00:12 +0200

    Update news.sh

--- news.sh
+++ news.sh
@@ -3,7 +3,7 @@
 gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
 gpmodel=$(sed -n 16p /etc/default/.hw_model)
 scriptv=$(sed -n 1p /etc/default/.script_version 2>/dev/null)
-srciptvd=94
+srciptvd=95
 scriptc="#02: fix Lan Led Rpi#03: fix backup/restore#04: fix ramsave"
 #
 #
@@ -124,6 +124,12 @@ echo -e " ───────────────────────�
 echo -e "$BGreen 24/05/22 Two experimental Embedded System versions released$Color_Off
 see
 http://gentooplayers.com/news
+"
+##############################################################################
+echo -e " ────────────────────────────────────────────"
+echo -e "$BGreen 02/06/22 Update diretta Host (alsaSync) v.105$Color_Off
+Fixed Target detection in host driver
+$BBlack use gp-update before trying the update$Color_Off
 "
 ##############################################################################
 
