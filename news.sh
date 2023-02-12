182d6242b5e19ca79a9ae965564b1c86c0829328
antonellocaroli <antonellocaroli@gmail.com>
2023-02-12 17:28:36 +0100

    18

--- news.sh
+++ news.sh
@@ -3,7 +3,7 @@
 gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
 gpmodel=$(sed -n 16p /etc/default/.hw_model)
 scriptv=$(sed -n 1p /etc/default/.script_version 2>/dev/null)
-srciptvd=133
+srciptvd=134
 scriptc="#02: fix Lan Led Rpi#03: fix backup/restore#04: fix ramsave"
 #
 #
@@ -230,7 +230,11 @@ fix bug CIFS in ramsystem not work
 kernel 6.1.1 not CLTO work with Allo UsbBridge
 "
 ##############################################################################
-
+echo -e " ────────────────────────────────────────────"
+echo -e "$BGreen 12/02/23 script v. 134:$Color_Off
+-update Script server
+"
+##############################################################################
 
 
 
