28893db9b7f043b7c1113dfd684616da7c002be1
antonellocaroli <antonellocaroli@gmail.com>
2023-10-14 10:55:48 +0200

    4

--- news.sh
+++ news.sh
@@ -3,7 +3,7 @@
 gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
 gpmodel=$(sed -n 16p /etc/default/.hw_model)
 scriptv=$(sed -n 1p /etc/default/.script_version 2>/dev/null)
-srciptvd=172
+srciptvd=173
 scriptc="#02: fix Lan Led Rpi#03: fix backup/restore#04: fix ramsave"
 #
 #
@@ -300,6 +300,11 @@ http://gentooplayers.com/news
 echo -e " ────────────────────────────────────────────"
 echo -e "$BGreen 08.10.23 Update Rpi Kernel$Color_Off
 solve led bug
+"
+##############################################################################
+echo -e " ────────────────────────────────────────────"
+echo -e "$BGreen 14.10.23 Update Rpi Kernel$Color_Off
+solve mount bug
 "
 ##############################################################################
 
