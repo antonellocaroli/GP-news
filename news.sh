14fe6d112b47a372a0d1a6d003c4589302bd9c2c
antonellocaroli <antonellocaroli@gmail.com>
2024-09-01 11:31:23 +0200

    Update news.sh

--- news.sh
+++ news.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 srciptvd=254
-host="139_1: \n-Target search Memory Broken in Host"
-target="139_1: \n-Target search Memory Broken in Host"
+host="  139_1:\n-Target search Memory Broken in Host"
+target="139_1:\n-Target search Memory Broken in Host"
 . /opt/.gentooplayer/function/felenco.sh
 gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
 gpmodel=$(sed -n 16p /etc/default/.hw_model)
