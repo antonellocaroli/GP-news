c0982f7c73e2982e303ec968a934841a4be205cd
antonellocaroli <antonellocaroli@gmail.com>
2024-09-24 05:01:49 +0200

    91

--- news.sh
+++ news.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-srciptvd=259
+srciptvd=260
 host="140_1:\n-Target bug fix\n-Problem detecting second USB-DAC"
 target="140_1:\n-Target bug fix\n-Problem detecting second USB-DAC"
 . /opt/.gentooplayer/function/felenco.sh
