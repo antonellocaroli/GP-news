c391f6ed9f091022894a64ac8c20882dcbd0896c
antonellocaroli <antonellocaroli@gmail.com>
2024-10-10 06:31:09 +0200

    2

--- news.sh
+++ news.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-srciptvd=269
+srciptvd=270
 host="140_1:\n-Fixed problems with the update process.\n-Problem of querying the server even when the update is not targeted."
 target="140_1:\n-Fixed problems with the update process.\n-Problem of querying the server even when the update is not targeted."
 . /opt/.gentooplayer/function/felenco.sh
