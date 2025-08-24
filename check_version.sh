efb234d75bd99786153ca3dd340c60851bd0feb6
antonellocaroli <antonellocaroli@gmail.com>
2025-08-24 07:01:23 +0200

    Update check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gpversionchek="$(sed -n 1p /etc/default/.GP-version)$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version | cut -c 1)"
-if [ "$gpversionchek" -lt 900 ]; then
+if [ "$gpversionchek" -lt 901 ]; then
   info "Install the latest version of GentooPlayer"
   fatal "$gpversionchek is no longer supported"
 fi
