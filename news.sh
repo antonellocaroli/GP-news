5b37586413f350658e513f5e8ce3034215e2962d
antonellocaroli <antonellocaroli@gmail.com>
2025-10-27 11:22:04 +0100

    Update news.sh

--- news.sh
+++ news.sh
@@ -514,11 +514,11 @@ echo -e "$BGreen #333$Color_Off
 echo -e " ────────────────────────────────────────────"
 echo -e "$BGreen 25/10/27 Update Kernel:$Color_Off
 info: include DDS driver updaten, previous kernels removed, news 25/10/25
--Rpi: 6.12.55-TEST/CLTO (251023)
--Rpi: 6.17.5-CLTO-TEST (251023)
+-Rpi: 6.12.55-TEST/CLTO (251026)
+-Rpi: 6.17.5-CLTO-TEST (251026)
 Unless there is a malfunction, these two versions should be compatible with
 allo-USBridgeSig
--x86_64: 6.17.5/CLTO (251022)
+-x86_64: 6.17.5/CLTO (251026)
 Update/Install/Remove > UP/DW kernel* New"
 ##############################################################################
 
